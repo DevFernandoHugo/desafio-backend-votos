158268597c7ce7613451178cce5eb0c249accaa3
Fernando Hugo <77555133+DevFernandoHugo@users.noreply.github.com>
2021-01-17 20:17:43 -0300

    Add files via upload

---
+++ sessao/src/test/java/com/desafio/sessao/SessaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.sessao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
