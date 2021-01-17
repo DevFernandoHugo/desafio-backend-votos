d12008371b6ec9f4753a427c00844ba8790257b6
DevFernandoHugo <fernandodeveloperlink@gmail.com>
2021-01-17 20:39:04 -0300

    add model pauta

---
+++ src/main/java/com/desafio/sessao/model/PautaVo.java
@@ -0,0 +1,13 @@
+package com.desafio.sessao.model;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class PautaVo implements Serializable{
+
+	private static final long serialVersionUID = -8412072079229667932L;
+	private String valor;
+
+}
